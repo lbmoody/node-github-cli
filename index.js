9e0e2b2eec08e331b1bd709415359404e9356b13
lmoody <l.moody@continuumonline.com>
2020-01-14 22:09:02 -0700

    updated index.js with questions for inquirer

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const pdf = require("html-pdf");
+
+const questions = [
+    {
+        type: "input"
+        , name: "username"
+        , message: "Enter your GitHub username"
+    },
+    {
+        type: "list"
+        , name: "color"
+        , message: "Which color do you want as the background?"
+        , choices: [
+            "Green"
+            , "Blue"
+            , "Pink"
+            , "Red"
+        ]
+    }
+];
 
 inquirer
-    .prompt({
-        message: "Enter your GitHub username",
-        name: "username"
-    })
-    .then(function ({ username }) {
+    .prompt(questions)
+    .then(function ({username, color}) {
         const queryUrl = `https://api.github.com/users/${username}`;
-
+        console.log(username, color);
         axios
             .get(queryUrl)
             .then(function (response) {
-                console.log(response.data);
+                const {avatar_url, login, location, html_url, blog, bio, public_repos, followers, following} = response.data;
             })
     });
+
+
+
+// data needed for app
+    // - Profile image                          avatar_url
+    // - User name                              login
+    // - Links to the following:
+    //     - User location via Google Maps      location
+    //     - User GitHub profile                html_url
+    //     - User blog                          blog
+    // - User bio                               bio
+    // - Number of public repositories          public_repos
+    // - Number of followers                    followers
+    // - Number of GitHub stars                 
+    // - Number of users following              following
